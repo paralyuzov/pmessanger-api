8d2bf951d1ad889f9ac35c16e8799f9f7724564d
Miroslav Paralyuzov <137619754+paralyuzov@users.noreply.github.com>
2025-10-28 01:52:30 +0200

    Add new feature modules and global config
    Integrated Auth, Socket, Friendship, Rooms, User, and Messages modules into the app. Enabled global configuration using ConfigModule with environment file support.

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,29 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PrismaModule } from './prisma/prisma.module';
+import { AuthModule } from './auth/auth.module';
+import { ConfigModule } from '@nestjs/config';
+import { SocketModule } from './socket/socket.module';
+import { FriendshipModule } from './friendship/friendship.module';
+import { RoomsModule } from './rooms/rooms.module';
+import { UserModule } from './user/user.module';
+import { MessagesModule } from './messages/messages.module';
 
 @Module({
-  imports: [PrismaModule],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: '.env',
+      cache: true,
+    }),
+    PrismaModule,
+    AuthModule,
+    SocketModule,
+    FriendshipModule,
+    RoomsModule,
+    UserModule,
+    MessagesModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
